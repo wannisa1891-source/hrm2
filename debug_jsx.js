02ce306fb1b81f682a1fd19d53fe483add14baa8
wannisa1891-source <wannisa1891@gmail.com>
2026-03-20 10:55:18 +0700

    ลบขยะ

--- debug_jsx.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const content = fs.readFileSync('src/app/employees/page.tsx', 'utf8');
-
-// Just print lines 200 to 360 with line numbers
-const lines = content.split('\n');
-for (let i = 200; i < 360; i++) {
-  console.log(`${i + 1}: ${lines[i]}`);
-}
--- get_emp.js
+++
@@ -1,5 +0,0 @@
-const fs=require('fs');
-const content=fs.readFileSync('D:/Intern/Web Dev/hrm_db.sql','utf-8');
-const m=content.match(/CREATE TABLE.*?tbl_employees[\s\S]*?ENGINE/i);
-if(m) console.log(m[0]);
-else console.log('NOT FOUND');
