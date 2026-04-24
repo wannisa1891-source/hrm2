aed018176b3ea16df5c07063c9c7aa53ae8db427
viewvi-25 <view1860@gmail.com>
2026-04-24 15:09:14 +0700

    ลบสันยลัก

--- src/components/Schedule/SchedulePage.tsx
+++ src/components/Schedule/SchedulePage.tsx
@@ -766,7 +766,6 @@ export default function SchedulePage() {
             <div className="sp-modal-box" onClick={(e) => e.stopPropagation()} style={{ width: '500px' }}>
               <div className="sp-modal-top">
                 <h3>
-                  <svg width="20" height="20" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
                   เพิ่มการเบิกงบประมาณประชุม/อบรม
                 </h3>
                 <button className="sp-modal-x" onClick={() => setShowReimModal(false)}>
